437a321a489c27500348e754c8b7e5290647e8c3
Xiaonan DONG <xndong@thoughtworks.com>
2020-07-23 09:09:06 +0800

    init zap, call from zap proxy

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Homework
+
+请使用OWASP ZAP作为漏洞扫描工具，在你自己的本地开发环境中实现自动化的动态应用安全测试。可以使用OWASP JuiceShop作为含有安全漏洞的目标测试应用。不强制要求加入BDD测试框架。请提交以下作业：
+
+1. 自动化动态应用安全测试的源代码或其截图，需要显示出源码调用了ZAP API。
+2. 经过自动化动态应用安全测试后，通过调用ZAP API生成的扫描结果（通过调用ZAP htmlreport API而生成的HTML报告，或者通过调用ZAP alerts_summary API而获取到的关于目标测试网站的Alerts Summary数据）
+
+
+[Juice Shop](https://juice-shop.herokuapp.com/)
+
+[OWASP ZAP + nodejs](https://github.com/zaproxy/zap-api-nodejs)
+
+
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t https://juice-shop.herokuapp.com -g gen.conf -r testreport.html
+
---
+++ search-juiceshop.js
@@ -0,0 +1,55 @@
+const http = require('http')
+const ZapClient = require('zaproxy')
+
+const zapDomain = "127.0.0.1"
+const zapPort = "8080"
+const juiceShopHost = "https://juice-shop.herokuapp.com"
+
+
+const zapOptions = {
+  proxy: `http://${zapDomain}:${zapPort}`
+};
+const zaproxy = new ZapClient(zapOptions);
+
+console.log("================= Step one - visit search page ======================")
+var opt = {
+ host: zapDomain,
+ port: zapPort,
+ method:'GET',//这里是发送的方法
+ path:`${juiceShopHost}/#/search?q=apple`    //这里是访问的路径
+}
+
+//以下是接受数据的代码
+var body = '';
+var req = http.request(opt, function(res) {
+  console.log("Got response: " + res.statusCode);
+  res.on('data',function(d){
+  body += d;
+ }).on('end', function(){
+  console.log(res.headers)
+  console.log(body)
+ });
+
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+})
+req.end();
+
+console.log("================= Step two - scan juice shop ======================")
+zaproxy.spider.scan(juiceShopHost, "", true, "Juice-shop", false, (err, resp) => {
+  // All errors are handled here
+  if (err) {
+      console.info('Error:',err);
+      let error = { statusCode: 422, error: err };
+      callback(error);
+  }
+  // All valid responses are handled here
+  if (resp) {
+      console.info('Response:',resp);
+      let result = resp.scan;
+      callback(null, result);
+  }
+});
+
+
+console.log("================= Step two - scan juice shop ======================")
